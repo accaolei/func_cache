81e952532f530be94b7a685ec4a56061d6cab408
caol <zmcl@sina.cn>
2020-02-14 11:40:08 +0800

    只支持Python3.5以上的版本

--- setup.py
+++ setup.py
@@ -24,13 +24,10 @@ setup(
     include_package_data=True,
     platforms='any',
     classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     test_suite='func_cache.tests',
 )
